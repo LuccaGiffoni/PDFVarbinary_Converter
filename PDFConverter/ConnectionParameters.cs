d42b279b71e800336475f067bf81ada5fb5a4dd9
Lucca Giffoni <luccagiffoni@outlook.com>
2021-09-30 16:39:04 -0300

    basic pdf converter

---
+++ PDFConverter/ConnectionParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFConverter
+{
+    class ConnectionParameters
+    {
+        public static string DataSource = "goit.database.windows.net";
+        public static string UserID = "lucca";
+        public static string Password = "JavaScript2002.";
+        public static string InitialCatalog = "biomedical";
+    }
+}
---
+++ PDFConverter/Program.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace PDFConverter
+{
+    class Program
+    {
+        private static string OriginalDataSource;
+        private static string OriginalUserID;
+        private static string OriginalPassword;
+        private static string OriginalInitialCatalog;
+        static void Main()
+        {
+            // Header
+            Console.Clear();
+            Console.WriteLine("Welcome to the PDF Converter!\nPlease, select what you wanna do:\n");
+            Console.WriteLine("1. Save PDF file to database");
+            Console.WriteLine("2. Change connection string\n");
+            Console.WriteLine("3. Close application\n");
+            var response = Console.ReadLine();
+            Console.WriteLine("____________________________________________________________________________________\n\n");
+
+            if(response != "1" && response != "2" && response != "3")
+            {
+                // Header
+                Console.Clear();
+                Console.WriteLine("Welcome to the PDF Converter!\nPlease, select what you wanna do:\n");
+                Console.WriteLine("1. Save PDF file to database\n");
+                Console.WriteLine("2. Change connection string");
+                response = Console.ReadLine();
+                Console.WriteLine("____________________________________________________________________________________\n\n");
+            }
+            switch (response)
+            {
+                case "1":
+                    SavePDF();
+                    break;
+
+                case "2":
+                    ChangeSettings();
+                    break;
+
+                case "3":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void ChangeSettings()
+        {
+            // Saving original settings
+            OriginalDataSource = ConnectionParameters.DataSource;
+            OriginalUserID = ConnectionParameters.UserID;
+            OriginalPassword = ConnectionParameters.Password;
+            OriginalInitialCatalog = ConnectionParameters.InitialCatalog;
+
+            // Header
+            Console.Clear();
+            Console.WriteLine("Welcome to the Settings!\nFollow the instruction bellow to change parameters:\n");
+            Console.WriteLine("____________________________________________________________________________________\n\n");
+
+            // Display actual variables
+            Console.WriteLine("Actual parameters:\n");
+            Console.WriteLine("Data Source: " + ConnectionParameters.DataSource);
+            Console.WriteLine("User ID: " + ConnectionParameters.UserID);
+            Console.WriteLine("Password: " + ConnectionParameters.Password);
+            Console.WriteLine("Initial Catalog: " + ConnectionParameters.InitialCatalog);
+
+            Console.WriteLine("\nIf you really wanna change something, press enter or press any key to go back to main menu:");
+            var readKey = Console.ReadKey();
+
+            if(readKey.Key != ConsoleKey.Enter)
+            {
+                Main();
+            }
+
+            while(readKey.Key == ConsoleKey.Enter)
+            {
+                // Display actual variables
+                Console.Clear();
+                Console.WriteLine("\nActual parameters:\n");
+                Console.WriteLine("Data Source: " + ConnectionParameters.DataSource);
+                Console.WriteLine("User ID: " + ConnectionParameters.UserID);
+                Console.WriteLine("Password: " + ConnectionParameters.Password);
+                Console.WriteLine("Initial Catalog: " + ConnectionParameters.InitialCatalog);
+
+                // Editing parameters
+                Console.WriteLine("\nChanging parameters:\n");
+                Console.WriteLine("Data Source:");
+                    string datasource = Console.ReadLine();
+                Console.WriteLine("\nUser ID:");
+                    string userid = Console.ReadLine();
+                Console.WriteLine("\nPassword:");
+                    string password = Console.ReadLine();
+                Console.WriteLine("\nInitial Catalog:");
+                    string initialcatalog = Console.ReadLine();
+
+                // Verifying response
+                Console.WriteLine("\nNew parameters:\n");
+                Console.WriteLine("Data Source: " + ConnectionParameters.DataSource);
+                Console.WriteLine("User ID: " + ConnectionParameters.UserID);
+                Console.WriteLine("Password: " + ConnectionParameters.Password);
+                Console.WriteLine("Initial Catalog: " + ConnectionParameters.InitialCatalog);
+
+                Console.WriteLine("\nTesting connection with new parameters...\n");
+                TestConnection(datasource, userid, password, initialcatalog);
+
+                Console.WriteLine("What you wanna do now: ");
+                Console.WriteLine("\n[ENTER] Change parameters again");
+                Console.WriteLine("[TAB] Reset to the very first original parameters");
+                Console.WriteLine("[SPACE] Go back to main menu");
+                Console.WriteLine("[BACKSPACE] Close application");
+
+                readKey = Console.ReadKey();
+            }
+
+            if(readKey.Key == ConsoleKey.Tab)
+            {
+                // Reseting to the original settings
+                ConnectionParameters.DataSource = OriginalDataSource;
+                ConnectionParameters.UserID = OriginalUserID;
+                ConnectionParameters.Password = OriginalPassword;
+                ConnectionParameters.InitialCatalog = OriginalInitialCatalog;
+
+                Console.WriteLine("Parameters are already set to original values!");
+                Console.WriteLine("\nTo go back to main menu press enter or any key to close app:");
+                var key = Console.ReadKey();
+
+                if(key.Key == ConsoleKey.Enter)
+                {
+                    Main();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+            else if(readKey.Key == ConsoleKey.Spacebar)
+            {
+                Main();
+            }
+            else if(readKey.Key == ConsoleKey.Backspace)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private static void TestConnection(string datasource, string userid, string password, string initialcatalog)
+        {
+            // Estabilishing Connection
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = datasource;
+            builder.UserID = userid;
+            builder.Password = password;
+            builder.InitialCatalog = initialcatalog;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    // Opening connection
+                    connection.Open();
+
+                    if (connection.State.ToString() == "Open")
+                    {
+                        ConnectionParameters.DataSource = datasource;
+                        ConnectionParameters.UserID = userid;
+                        ConnectionParameters.Password = password;
+                        ConnectionParameters.InitialCatalog = initialcatalog;
+
+                        Console.WriteLine("\nThe connection was successfully estabilished and the parameters were change!");
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+
+                ConnectionParameters.DataSource = datasource;
+                ConnectionParameters.UserID = userid;
+                ConnectionParameters.Password = password;
+                ConnectionParameters.InitialCatalog = initialcatalog;
+
+                Console.WriteLine("\nThe connection failed but the parameters were change");
+            }
+        }
+
+        private static void SavePDF()
+        {
+            // Header
+            Console.Clear();
+            Console.WriteLine("Welcome to the PDF Converter!\nPlease, follow the instructions above:\n");
+
+            // Get filename
+            Console.WriteLine("Please, enter the name of the file: ");
+            string pdfname = Console.ReadLine();
+
+            // Test if the file termination is ok
+            while (!pdfname.Contains(".pdf"))
+            {
+                Console.WriteLine("\nPlease, enter the name again using the pdf file termination (.pdf): ");
+                pdfname = Console.ReadLine();
+            }
+
+            // Get filepath
+            Console.WriteLine("\nPlease, enter the filepath: ");
+            string filepath = Console.ReadLine();
+
+            // Start process
+            Console.WriteLine("\n\nPress any key to convert the PDF to varbinary and save to database");
+            Console.ReadKey();
+
+            // Estabilish connection with database
+            OpenConnection(pdfname, filepath);
+        }
+
+        private static void OpenConnection(string pdfname, string filepath)
+        {
+            // Estabilishing Connection
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = ConnectionParameters.DataSource;
+            builder.UserID = ConnectionParameters.UserID;
+            builder.Password = ConnectionParameters.Password;
+            builder.InitialCatalog = ConnectionParameters.InitialCatalog;
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                // Opening connection
+                connection.Open();
+
+                // Setting the file and the filename to be saved
+                FileStream fStream = File.OpenRead(filepath);
+
+                // Writing the doc as a stream
+                byte[] contents = new byte[fStream.Length];
+                fStream.Read(contents, 0, (int)fStream.Length);
+                fStream.Close();
+
+                try
+                {
+                    // Insert the converted PDF to database
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO archives VALUES (@fileContent, @fileName)", connection))
+                    {
+                        cmd.Parameters.AddWithValue("@fileName", pdfname);
+                        cmd.Parameters.AddWithValue("@fileContent", contents);
+                        cmd.ExecuteNonQuery();
+
+                        Console.WriteLine("The PDF was saved with success on database");
+                        Console.WriteLine("\nTo go back to main menu, press enter or press any other key to close application:");
+                        var readKey = Console.ReadKey();
+
+                        if(readKey.Key == ConsoleKey.Enter)
+                        {
+                            Main();
+                        }
+                        else
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+
+                    Console.WriteLine("\nAn error ocurred, to go back to main menu and try again, press enter or any other key to close application:");
+                    var readKey = Console.ReadKey();
+
+                    if(readKey.Key == ConsoleKey.Enter)
+                    {
+                        Main();
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
